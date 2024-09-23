4b9699febb92e9f016f061e8928f76eb1eca79a6
Letícia Magnan <100086541+yurinotfound404@users.noreply.github.com>
2024-09-22 23:23:38 -0300

    add section banner

--- script.js
+++ script.js
@@ -2,3 +2,26 @@ window.addEventListener('scroll', function() { // Use quotes around 'scroll'
     var header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+
+const slides = document.querySelectorAll('.slide');
+let currentIndex = 0;
+
+// Function to update the carousel
+function updateCarousel() {
+    slides.forEach((slide, index) => {
+        slide.classList.remove('active');
+        if (index === currentIndex) {
+            slide.classList.add('active');
+        }
+    });
+}
+
+// Automatic scrolling
+setInterval(() => {
+    currentIndex = (currentIndex + 1) % slides.length; // Cycle through slides
+    updateCarousel();
+}, 3000); // Change slide every 5 seconds
+
+// Initial update to set the first slide
+updateCarousel();
